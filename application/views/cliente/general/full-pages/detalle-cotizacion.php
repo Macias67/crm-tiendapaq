54b93f32deeca71bdd04719755c256f94f311e65
Luis Macias <luismacias.angulo@gmail.com>
2014-10-21 20:04:19 -0500

    Controlador para trabjar las cotizaciones del cliente

---
+++ application/views/cliente/general/full-pages/detalle-cotizacion.php
@@ -0,0 +1,12 @@
+<div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+	<h4 class="modal-title"><b>Detalles del pendiente: </b></h4>
+</div>
+<div class="modal-body">
+	<div class="row">
+	<?php echo $folio ?>
+	</div>
+</div>
+<div class="modal-footer">
+	<button type="button" class="btn green update">Aceptar</button>
+</div>
