3d263a14dbd94c061bd9374f8dc91c97e5f6db32
Diego I. Rodriguez <ing.diegorod@gmail.com>
2014-11-06 18:48:20 -0600

    la vida es bella

--- application/controllers/ejecutivo/pendiente.php
+++ application/controllers/ejecutivo/pendiente.php
@@ -125,7 +125,7 @@ class Pendiente extends AbstractAccess {
 
 		// SI la actividad es COTIZAR
 		if ($pendiente->id_actividad_pendiente == $this->actividadPendienteModel->SOLICITA_COTIZACION) {
-			$this->data['url_cotiza']= anchor('cotizador/'.$pendiente->id_actividad_pendiente, 'Cotizar', 'class="btn red"');
+			$this->data['url_cotiza']= anchor('cotizador/'.$pendiente->id_pendiente, 'Cotizar', 'class="btn red"');
 		}
 
 		$this->_vista_completa('detalle-pendiente');
