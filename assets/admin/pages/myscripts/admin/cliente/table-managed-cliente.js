81e35a61f531fdb12a91ffb02a9ea981e35a37b3
Diego I. Rodriguez <ing.diegorod@gmail.com>
2014-12-18 15:52:20 -0600

    modificaciones menores

--- assets/admin/pages/myscripts/admin/cliente/table-managed-cliente.js
+++ assets/admin/pages/myscripts/admin/cliente/table-managed-cliente.js
@@ -47,13 +47,15 @@ var TableManagedCliente = function() {
 				$('td:eq(0)', nRow).html('<input type="checkbox" class="checkboxes" '+checkbox+'/>');
 				// Tipo de Cliente
 				if (aData.tipo == "Normal") {
-					$('td:eq(5)', nRow).html('<span class="badge badge-info badge-roundless"><b>'+aData.tipo+'</b></span>');
+					$('td:eq(5)', nRow).html('<span class="badge badge-success"><b>'+aData.tipo+'</b></span>');
 				} else if (aData.tipo == "Prospecto") {
-					$('td:eq(5)', nRow).html('<span class="badge badge-success badge-roundless"><b>'+aData.tipo+'</b></span>');
+					$('td:eq(5)', nRow).html('<span class="badge badge-warning"><b>'+aData.tipo+'</b></span>');
+				} else if(aData.tipo == "Distribuidor"){
+					$('td:eq(5)', nRow).html('<span class="badge badge-danger"><b>'+aData.tipo+'</b></span>');
 				}
 				// Enlace a la edicion
 				var id  = $(nRow).attr('id');
-				$('td:eq(6)', nRow).html('<a type="button" href="/cliente/gestionar/editar/'+id+'" class="btn btn-circle green btn-xs"><i class="fa fa-search-plus"></i> Ver/Editar</a>');
+				$('td:eq(6)', nRow).html('<a type="button" href="/cliente/gestionar/editar/'+id+'" class="btn btn-circle blue btn-xs"><i class="fa fa-search"></i> Ver/Editar</a>');
 			},
 			"drawCallback": function(settings) {
 				Metronic.initUniform($('input[type="checkbox"]', table)); // reinitialize uniform checkboxes on each table reload
