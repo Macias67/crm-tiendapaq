f7a9a57509a083316713deb3de9370fab7219c44
Luis Macias <luismacias.angulo@gmail.com>
2015-01-26 15:40:35 -0600

    Plantilla de envio de cotizacion listo

--- application/views/admin/general/full-pages/email/email_envio_cotizacion.php
+++ application/views/admin/general/full-pages/email/email_envio_cotizacion.php
@@ -9,78 +9,6 @@
 		<table class="body">
 		<tr>
 			<td class="center" align="center" valign="top">
-				<!-- BEGIN: Header -->
-				<table class="page-header" align="center" style="width: 100%;background: #1f1f1f;">
-				<tr>
-					<td class="center" align="center">
-						<!-- BEGIN: Header Container -->
-						<table class="container" align="center">
-						<tr>
-							<td>
-								<table class="row ">
-								<tr>
-									<td class="wrapper vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;">
-										<!-- BEGIN: Logo -->
-										<table class="six columns">
-										<tr>
-											<td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;">
-												<a href="index.html">
-												<img src="<?php echo $assets_admin_layout ?>img/logo.png" width="86" height="14" border="0" alt="" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;height: auto;max-width: 100%;float: left;clear: both;display: block;">
-												</a>
-											</td>
-										</tr>
-										</table>
-										<!-- END: Logo -->
-									</td>
-									<td class="wrapper vertical-middle last" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;">
-										<!-- BEGIN: Social Icons -->
-										<table class="six columns">
-										<tr>
-											<td>
-												<table class="wrapper social-icons" align="right" style="float: right;">
-												<tr>
-													<td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;">
-														<a href="#">
-														<img src="<?php echo $assets_admin_pages ?>media/email/social_facebook.png" alt="social icon" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;height: auto;max-width: none !important;float: left;clear: both;display: block;">
-														</a>
-													</td>
-													<td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;">
-														<a href="#">
-														<img src="<?php echo $assets_admin_pages ?>media/email/social_twitter.png" alt="social icon" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;height: auto;max-width: none !important;float: left;clear: both;display: block;">
-														</a>
-													</td>
-													<td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;">
-														<a href="#">
-														<img src="<?php echo $assets_admin_pages ?>media/email/social_googleplus.png" alt="social icon" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;height: auto;max-width: none !important;float: left;clear: both;display: block;">
-														</a>
-													</td>
-													<td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;">
-														<a href="#">
-														<img src="<?php echo $assets_admin_pages ?>media/email/social_linkedin.png" alt="social icon" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;height: auto;max-width: none !important;float: left;clear: both;display: block;">
-														</a>
-													</td>
-													<td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;padding-right: 0 !important;">
-														<a href="#">
-														<img src="<?php echo $assets_admin_pages ?>media/email/social_rss.png" alt="social icon" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;height: auto;max-width: none !important;float: left;clear: both;display: block;">
-														</a>
-													</td>
-												</tr>
-												</table>
-											</td>
-										</tr>
-										</table>
-										<!-- END: Social Icons -->
-									</td>
-								</tr>
-								</table>
-							</td>
-						</tr>
-						</table>
-						<!-- END: Header Container -->
-					</td>
-				</tr>
-				</table>
-				<!-- END: Header -->
 				<!-- BEGIN: Content -->
 				<table class="container content" align="center">
 				<tr>
@@ -88,23 +16,36 @@
 						<table class="row note">
 						<tr>
 							<td class="wrapper last">
-								<h4 style="font-size: 22px;display: block;margin: 5px 0 15px 0;">Thank you for joining the Metronic Community!</h4>
+								<h4 style="font-size: 22px;display: block;margin: 5px 0 15px 0;"> TiendaPAQ - DISTRIBUIDOR ASOCIADO MASTER CONTPAQi </h4>
 								<p>
-									 Please click the following URL to activate your account:
+									 Adjuntamos a la cotización sus datos de inicio de sesión en nuestro sistema, aquí podrá verificar sus pagos de la cotización.
 								</p>
+								<!-- BEGIN: Note Panel -->
+								<table class="twelve columns" style="margin-bottom: 10px">
+									<tr>
+										<td class="panel" style="background: #ECF8FF;border: 0;padding: 10px !important;">
+											<b>Usuario:</b> <?php echo $usuario ?>
+										</td>
+									</tr>
+									<tr>
+										<td class="panel" style="background: #ECF8FF;border: 0;padding: 10px !important;">
+											<b>Contraseña:</b>  <?php echo $password ?>
+										</td>
+									</tr>
+								</table>
 								<!-- BEGIN: Note Panel -->
 								<table class="twelve columns" style="margin-bottom: 10px">
 								<tr>
 									<td class="panel" style="background: #ECF8FF;border: 0;padding: 10px !important;">
-										<a href="https://www.keenthemes.com/signup/activate/">
-										https://www.keenthemes.com/signup/activate/?code=1212 </a>
+										<a href="<?php echo base_url() ?>">
+										<?php echo base_url() ?> </a>
 									</td>
 									<td class="expander">
 									</td>
 								</tr>
 								</table>
 								<p>
-									 If clicking the URL above does not work, copy and paste the URL into a browser window.
+									Si al hacer clic en la URL de arriba no funciona, copia y pega la URL en el navegador.
 								</p>
 								<!-- END: Note Panel -->
 							</td>
@@ -119,11 +60,13 @@
 								<table class="twelve columns">
 								<tr>
 									<td>
-										<h4 style="font-size: 22px;display: block;margin: 5px 0 15px 0;">70% Disscount!</h4>
+										<img src="<?php echo $assets_admin_pages.'media/email/LogoTiendaPAQ.gif' ?>" class="ie10-responsive" alt="" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;height: auto;max-width: 100%;float: left;clear: both;display: block;">
+									</td>
+									<td>
+										<h4 style="font-size: 18px;display: block;margin: 5px 0 15px 0;"> Soporte técnico y capacitación. </h4>
 										<p>
-											 Phasellus dictum sapien a neque luctus cursus. Pellentesque sem dolor, fringilla et pharetra vitae. Pellentesque sem dolor, fringilla et pharetra vitae
+											 Reciba un cordial saludo y nuestro agradecimiento por su confianza e interes en los Productos y Servicios que ofrecemos.
 										</p>
-										<img src="<?php echo $assets_admin_pages ?>media/email/article.png" class="ie10-responsive" alt="" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;height: auto;max-width: 100%;float: left;clear: both;display: block;">
 									</td>
 									<td class="expander">
 									</td>
@@ -149,7 +92,7 @@
 								<tr>
 									<td class="wrapper last" style="vertical-align: middle;color: #fff;">
 										<span style="font-size:12px;">
-										<i>This ia a system generated email and reply is not required.</i>
+										<i>Email generado por el sistema, no reenviar.</i>
 										</span>
 									</td>
 								</tr>
@@ -157,31 +100,22 @@
 								<!-- END: Unsubscribe -->
 								<!-- BEGIN: Footer Panel -->
 								<table class="row">
-								<tr>
-									<td class="wrapper" style="vertical-align: middle;color: #fff;">
-										<table class="four columns">
-										<tr>
-											<td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;color: #fff;">
-												 &copy; Keenthemes 2013.
-											</td>
-										</tr>
-										</table>
-									</td>
-									<td class="wrapper last" style="vertical-align: middle;color: #fff;">
-										<table class="eight columns">
-										<tr>
-											<td class="vertical-middle align-reverse" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;text-align: right;color: #fff;">
-												<a href="#">
-												About Us </a>
-												&nbsp; <a href="#">
-												Privacy Policy </a>
-												&nbsp; <a href="#">
-												Terms of Use </a>
-											</td>
-										</tr>
-										</table>
-									</td>
-								</tr>
+									<tr>
+										<td class="wrapper" style="vertical-align: middle;color: #fff;">
+											<table class="four columns">
+											<tr>
+												<td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;padding: 0 2px !important;width: auto !important;">
+													<a href="https://www.facebook.com/Tiendapaq">
+													<img src="<?php echo $assets_admin_pages.'media/email/social_facebook.png' ?>" alt="social icon" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;height: auto;max-width: none !important;float: left;clear: both;display: block;">
+													</a>
+												</td>
+												<td class="vertical-middle" style="padding-top: 0;padding-bottom: 0;vertical-align: middle;color: #fff;">
+													 &copy; TiendaPAQ 2015.
+												</td>
+											</tr>
+											</table>
+										</td>
+									</tr>
 								</table>
 								<!-- END: Footer Panel List -->
 							</td>
