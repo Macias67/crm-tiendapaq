e10a73faf673ab0b90a43df5a34a9c75d3dd3c5d
Diego I. Rodriguez <ing.diegorod@gmail.com>
2015-01-28 15:11:49 -0600

    cambio menor

--- application/views/cliente/cotizacion/container/archivos.php
+++ application/views/cliente/cotizacion/container/archivos.php
@@ -5,7 +5,7 @@
 				<div class="row">
 					<div class="col-md-12">
 						<!-- BEGIN PAGE TITLE & BREADCRUMB-->
-						<h3 class="page-title">Cotización - <small></small></h3>
+						<h3 class="page-title">Cotización - <?php echo $cotizacion->folio ?> - <small><?php echo $cotizacion->razon_social ?></small></h3>
 						<!-- END PAGE TITLE & BREADCRUMB-->
 					</div>
 				</div>
