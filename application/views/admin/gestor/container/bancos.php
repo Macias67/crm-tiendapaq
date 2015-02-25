5e2908b608362ef7baed89ec0d8d6388faac65cb
gatsuxolotl <david_barajas_venegas@hotmail.com>
2015-02-25 14:39:31 -0600

    modifique la vista de bancos y la vista de observaciones, de un portlet gray a un portlet light

--- application/views/admin/gestor/container/bancos.php
+++ application/views/admin/gestor/container/bancos.php
@@ -14,55 +14,49 @@
 		<!-- BEGIN PAGE CONTENT-->
 		<div class="row">
 			<div class="col-md-12">
-				<div class="portlet box grey">
-					<div class="portlet-title">
-						<div class="caption" style="color: black">
-							<i class="fa fa-usd"></i> Bancos
-						</div>
-						<div class="tools" style="color: black">
-							<a href="javascript:;" class="collapse">
-							</a>
-							<a href="javascript:;" class="reload">
-							</a>
-						</div>
-					</div>
-					<div class="portlet-body">
-						<table class="table table-striped table-hover table-bordered" id="tabla_bancos_editable">
-							<thead>
-								<tr>
-									<th>Banco</th>
-									<th>Sucursal</th>
-									<th>Numero de cuenta</th>
-									<th>Titular</th>
-									<th>Clave interbancaria</th>
-									<th></th>
-									<th></th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php foreach ($bancos as $banco): ?>
-									<tr id="<?php echo $banco->id_banco?>">
-										<td><?php echo $banco->banco ?></td>
-										<td><?php echo $banco->sucursal ?></td>
-										<td><?php echo $banco->cta ?></td>
-										<td><?php echo $banco->titular ?></td>
-										<td><?php echo $banco->cib ?></td>
-										<td><a class="btn edit blue btn-circle btn-xs" href="<?php echo site_url('gestor/bancos/mostrar/'.$banco->id_banco) ?>" data-target="#ajax_editar_banco" data-toggle="modal"><i class="fa fa-edit"></i> Ver/Editar </a></td>
-										<td><a class="btn delete red btn-circle btn-xs eliminar-banco" href="javascript:;"><i class="fa fa-trash"></i> Eliminar </a></td>
-									</tr>
-								<?php endforeach ?>
-							</tbody>
-						</table>
-						<br>
-						<div class="table-toolbar">
-							<div class="btn-group pull-right">
-								<a id="btn_nuevo_banco" class="btn btn-circle green btn-xs" href="#modal_nuevo_banco" data-toggle="modal">
-									<i class="fa fa-plus"></i> Nuevo banco
-								</a>
+				<!-- BEGIN TABLA BANCOS -->
+				<div class="col-md-12">
+					<div class="portlet light">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-usd"></i>
+								<span class="caption-subject bold uppercase"> Bancos</span>
 							</div>
+							<div class="actions">
+								<a class="btn btn-circle green btn-xs" href="#modal_nuevo_banco" data-toggle="modal"><i class="fa fa-plus"></i> Nuevo banco</a>
+							</div>
+						</div>
+						<div class="portlet-body">
+							<table class="table table-striped table-hover table-bordered" id="tabla_bancos_editable">>
+								<thead>
+									<tr>
+										<th>Banco</th>
+										<th>Sucursal</th>
+										<th>Numero de cuenta</th>
+										<th>Titular</th>
+										<th>Clave interbancaria</th>
+										<th></th>
+										<th></th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach ($bancos as $banco) : ?>
+										<tr id="<?php echo $banco->id_banco?>">
+											<td><?php echo $banco->banco ?></td>
+											<td><?php echo $banco->sucursal ?></td>
+											<td><?php echo $banco->cta ?></td>
+											<td><?php echo $banco->titular ?></td>
+											<td><?php echo $banco->cib ?></td>
+											<td><a class="btn edit blue btn-circle btn-xs" href="<?php echo site_url('gestor/bancos/mostrar/'.$banco->id_banco) ?>" data-target="#ajax_editar_banco" data-toggle="modal"><i class="fa fa-edit"></i> Ver/Editar </a></td>
+											<td><a class="btn delete red btn-circle btn-xs eliminar-banco" href="javascript:;"><i class="fa fa-trash"></i> Eliminar </a></td>
+										</tr>
+									<?php endforeach ?>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
+				<!-- END TABLA BANCOS -->
 			</div>
 		</div>
 		<!-- END PAGE CONTENT-->
--- application/views/admin/gestor/container/observaciones.php
+++ application/views/admin/gestor/container/observaciones.php
@@ -17,48 +17,41 @@
 					<div class="col-md-12">
 						<div class="col-md-2">
 						</div>
-						<div class="col-md-8">
-							<div class="portlet box grey">
-								<div class="portlet-title">
-									<div class="caption" style="color: black">
-										<i class="fa fa-windows"></i> Observaciones
-									</div>
-									<div class="tools" style="color: black">
-										<a href="javascript:;" class="collapse">
-										</a>
-										<a href="javascript:;" class="reload">
-										</a>
-									</div>
-								</div>
-								<div class="portlet-body">
-									<table class="table table-striped table-hover table-bordered" id="tabla_observaciones_editable">
-										<thead>
-											<tr>
-												<th>Descripcion</th>
-												<th></th>
-												<th></th>
-											</tr>
-										</thead>
-										<tbody>
-											<?php foreach ($observaciones as $observacion): ?>
-												<tr id="<?php echo $observacion->id_observacion?>">
-													<td><?php echo $observacion->descripcion ?></td>
-													<td><a class="btn edit blue btn-circle btn-xs" href="javascript:;"><i class="fa fa-edit"></i> Editar </a></td>
-													<td><a class="btn delete red btn-circle btn-xs" href="javascript:;"><i class="fa fa-trash"></i> Eliminar </a></td>
-												</tr>
-											<?php endforeach ?>
-										</tbody>
-									</table>
-									<div class="table-toolbar">
-										<div class="btn-group pull-right">
-											<button id="tabla_observaciones_editable_new" class="btn btn-circle green btn-xs">
-												<i class="fa fa-plus"></i> Nueva observación
-											</button>
-										</div>
-									</div>
-								</div>
+				<!-- BEGIN TABLA OBSERVACIONES -->
+				<div class="col-md-8">
+					<div class="portlet light">
+						<div class="portlet-title">
+							<div class="caption">
+								<i class="fa fa-windows"></i>
+								<span class="caption-subject bold uppercase"> Observaciones</span>
+							</div>
+							<div class="actions">
+								<a class="btn btn-circle green btn-xs" href="#modal_nueva_observacion" data-toggle="modal"><i class="fa fa-plus"></i> Nueva observacion</a>
 							</div>
 						</div>
+						<div class="portlet-body">
+							<table class="table table-striped table-hover table-bordered" id="tabla_observaciones_editable">
+								<thead>
+									<tr>
+										<th>Descripcion</th>
+										<th></th>
+										<th></th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach ($observaciones as $observacion) : ?>
+										<tr id="<?php echo $observacion->id_observacion?>">
+											<td><?php echo $observacion->descripcion ?></td>
+											<td><a class="btn edit blue btn-circle btn-xs" href="javascript:;"><i class="fa fa-edit"></i> Editar </a></td>
+											<td><a class="btn delete red btn-circle btn-xs" href="javascript:;"><i class="fa fa-trash"></i> Eliminar </a></td>
+										</tr>
+									<?php endforeach ?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				</div>
+				<!-- END TABLA OBSERVACIONES -->
 						<div class="col-md-2">
 						</div>
 					</div>
