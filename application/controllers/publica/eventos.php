9a517dff26cd50d50197a580f69330243fb49a27
Luis Macias <luismacias.angulo@gmail.com>
2015-04-14 09:07:04 -0500

    Cambios en AbstractCOntroller, tabla gestion eventos

---
+++ application/controllers/publica/eventos.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Eventos extends AbstractController {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file eventos.php */
+/* Location: ./application/controllers/publica/eventos.php */
