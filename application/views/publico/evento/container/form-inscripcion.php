d84d86f7a22239078e1f516515a1ea9fadc6fa8b
Julio Trujillo <julio.trujillo.alvarado@outlook.com>
2015-04-17 14:51:30 -0500

    pequeños bugs y actualizo lista de pendientes eventos

--- application/views/publico/evento/container/form-inscripcion.php
+++ application/views/publico/evento/container/form-inscripcion.php
@@ -45,6 +45,12 @@
 							<p><?php echo ($evento->costo == 0) ? 'Sin Costo' : '$ '.number_format($evento->costo, 2, '.', ',').' MN'  ?></p>
 							<h5>Descripción: </h5>
 							<p><?php echo $evento->descripcion; ?></p>
+							<h5>Modalidad: </h5>
+							<p><?php echo ucwords($evento->modalidad); ?></p>
+							<h5>Cupo: </h5>
+							<p><?php echo ($evento->max_participantes == 0) ? 'Sin límite' : $evento->max_participantes.' personas' ?></p>
+							<h5>Temario:</h5>
+							<a href="<?php echo $temario_url ?>" target="_blank"><img src="<?php echo $temario_url ?>" class="img-responsive" alt=""></a>
 						</div>
 					</div>
 					<!-- END PORTLET-->
