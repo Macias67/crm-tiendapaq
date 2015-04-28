45cb9da90a9446166c153f3fcb4e36e17a40c33b
gatsuxolotl <david_barajas_venegas@hotmail.com>
2015-04-28 16:50:52 -0500

    cambio menor

--- application/config/constants.php
+++ application/config/constants.php
@@ -8,7 +8,7 @@
 | Ya sea local o vía remoto
 |
 */
-if ($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
+if ($_SERVER['SERVER_ADDR'] == '127.0.0.2') {
 	define('LOCAL', TRUE);
 } else {
 	define('LOCAL', FALSE);
